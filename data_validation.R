e54535170cd3736939b5d3fc0d60f2ee6a0ed00d
Emilio M. Bruna <embruna@ufl.edu>
2021-06-25 17:25:27 -0400

    formatting edits

--- data_validation.R
+++ data_validation.R
@@ -3,7 +3,8 @@
 
 #' Author: Eric Scott
 #' This uses pointblank to create a data validation report.
-#' In the resulting table at the end, any failing tests should have a CSV button that lets you see just the rows of data that don't pass.
+#' In the resulting table at the end, any failing tests should have a CSV button
+#' that lets you see just the rows of data that don't pass.
 #'
 
 # Packages ----------------------------------------------------------------
@@ -32,8 +33,20 @@ ha %>%
   create_agent(
     actions = al
   ) %>%
-  col_is_character(vars(plot, habitat, ranch, bdffp_reserve_no, row, code.notes, code2)) %>%
-  col_is_numeric(vars(HA_ID_Number, tag_number ,column, year, ht, shts, infl)) %>%
+  col_is_character(vars(plot, 
+                        habitat, 
+                        ranch, 
+                        bdffp_reserve_no, 
+                        row, 
+                        code.notes, 
+                        code2)) %>%
+  col_is_numeric(vars(HA_ID_Number, 
+                      tag_number,
+                      column, 
+                      year, 
+                      ht, 
+                      shts, 
+                      infl)) %>%
   col_vals_in_set(vars(column), 1:10) %>%
   col_vals_in_set(vars(row), LETTERS[1:10]) %>%
   col_vals_in_set(vars(habitat), c("1-ha", "CF", "10-ha")) %>%
@@ -42,8 +55,11 @@ ha %>%
   col_vals_between(vars(ht), 0, 200, na_pass = TRUE) %>%
   col_vals_between(vars(infl), 0, 3, na_pass = TRUE) %>%
   # check precision is correct
-  col_vals_expr(label = "Height is measured to nearest cm", expr = ~ ht %% 1 == 0) %>%
-  col_vals_expr(label = "Shoots is interger", expr = ~ shts %% 1 == 0) %>%
-  col_vals_expr(label = "Number of inflorescences is integer", expr = ~ infl %% 1 == 0) %>%
+  col_vals_expr(label = "Height is measured to nearest cm", 
+                expr = ~ ht %% 1 == 0) %>%
+  col_vals_expr(label = "Shoots is interger", 
+                expr = ~ shts %% 1 == 0) %>%
+  col_vals_expr(label = "Number of inflorescences is integer", 
+                expr = ~ infl %% 1 == 0) %>%
   rows_distinct() %>%
   interrogate()
