e644126e226b1cf4209d7920b5359f5f492422ea
Aariq <scottericr@gmail.com>
2021-07-27 14:55:28 -0400

    renamed intermediates for easier debugging

--- code_data_cleaning/merge_with_PA10.R
+++ code_data_cleaning/merge_with_PA10.R
@@ -5,13 +5,13 @@ merge_with_PA10 <- function(test) {
   PA10$column <-as.factor(PA10$column)
   PA10$plot <-as.factor(PA10$plot)
   PA10$bdffp_reserve_no <-as.character(PA10$bdffp_reserve_no)
-  test <- bind_rows(test,PA10)
+  test2 <- bind_rows(test,PA10)
   
   
   # add a unique id number for the PA10
   # max_ha_id<-max(test$HA_ID_Number,na.rm=TRUE)
-  test <- test %>%
+  test3 <- test2 %>%
     group_by(plotID,tag_number) %>%
     mutate(HA_ID_Number = ifelse(is.na(HA_ID_Number), group_indices(),HA_ID_Number))
-  return(test)
+  return(test3)
 }
