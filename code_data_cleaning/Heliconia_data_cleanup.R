1adeab8c0a02467be3a085db04054349b0346610
embruna <embruna@ufl.edu>
2021-10-27 17:38:56 -0400

    corrected "Florestal" to "Florestal-CF" on seedling changes

--- code_data_cleaning/Heliconia_data_cleanup.R
+++ code_data_cleaning/Heliconia_data_cleanup.R
@@ -572,41 +572,41 @@ ha_data$ht[ha_data$plot == "Florestal-CF" &
              ha_data$tag_number == 590] <- 49
 
 # incorrectly recorded as seedling
-ha_data$code[ha_data$plot == "Florestal" &
+ha_data$code[ha_data$plot == "Florestal-CF" &
                ha_data$year == 2008 & 
                ha_data$tag_number == 1916] <- "ULY (3)"
 
 # incorrectly recorded the ht as 73 (it is 7.3). Rounded down.
-ha_data$ht[ha_data$plot == "Florestal" &
+ha_data$ht[ha_data$plot == "Florestal-CF" &
                ha_data$year == 2001 & 
                ha_data$tag_number == 967] <- 7
 
 
 # incorrectly recorded as seedling
-ha_data$code[ha_data$plot == "Florestal" &
+ha_data$code[ha_data$plot == "Florestal-CF" &
                ha_data$year == 2007 & 
                ha_data$tag_number == 378] <- "ULY (3)"
 
 # incorrectly recorded as seedling
-ha_data$code[ha_data$plot == "Florestal" &
+ha_data$code[ha_data$plot == "Florestal-CF" &
                ha_data$year == 2007 & 
                ha_data$tag_number == 378] <- "ULY (3)"
 
 # incorrectly recorded as seedling ("nova com cara de velha")
-ha_data$code[ha_data$plot == "Florestal" &
+ha_data$code[ha_data$plot == "Florestal-CF" &
                ha_data$year == 2007 & 
                ha_data$tag_number == 1578] <- "ULY (3)"
 
 
 
 # correction (found after referring to the datasheet to fix 378 above)
-ha_data$shts[ha_data$plot == "Florestal" &
+ha_data$shts[ha_data$plot == "Florestal-CF" &
              ha_data$year == 2007 & 
              ha_data$tag_number == 1290] <- 2
-ha_data$ht[ha_data$plot == "Florestal" &
+ha_data$ht[ha_data$plot == "Florestal-CF" &
                ha_data$year == 2007 & 
                ha_data$tag_number == 1290] <- 16
-ha_data$code[ha_data$plot == "Florestal" &
+ha_data$code[ha_data$plot == "Florestal-CF" &
               ha_data$year == 2007 & 
               ha_data$tag_number == 1290] <- NA
 
