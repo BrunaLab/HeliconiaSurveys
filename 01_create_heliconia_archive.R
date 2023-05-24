4aeaab4a100d34cf67e780a813d211ee1461df60
embruna <embruna@ufl.edu>
2023-05-24 17:50:13 -0400

    update formatting

--- 01_create_heliconia_archive.R
+++ 01_create_heliconia_archive.R
@@ -7,6 +7,8 @@ source("./code/code_cleaning/clean_heliconia_data.R")
 ha_data<-clean_heliconia_data()
 
 
+
+
 # Step 2: Review of the 'Clean' Data ----------------------------------------
 
 # must have a `heliconia_data_clean` in the `data/data_clean` folder
@@ -18,23 +20,22 @@ review_heliconia_data()
 
 # Step 3: Create Files for the Data Archive ---------------------------------
 
-# csv file with records of file for Dryad ----------------------------
+# csv file of demog dataset for Dryad ---------------------------------------
 source("./code/code_archive/create_dryad_file.R")
 create_dryad_file()
 
-# create csv file with plot descriptors -------------------------------
+# csv file of plot descriptors ---------------------------------------------
 source("./code/code_archive/create_plot_info_file.R")
 create_plot_info_file()
 
-
-# csv file with treefall records--------------------------------------
+# csv file of treefall records ---------------------------------------------
 source("./code/code_archive/create_treefall_records_file.R")
 create_treefall_records_file()
 
-
-# csv file with records of tag replacements in the field -------------
+# csv file of tag replacements in the field --------------------------------
 source("./code/code_archive/create_tag_changes_file.R")
 create_tag_changes_file()
+
 
 
 
